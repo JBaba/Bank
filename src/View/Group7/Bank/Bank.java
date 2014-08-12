510edaa7c71661cf9f181f38f80dcdc75038e421
Naimish Viradia <naimish.viradia@yahoo.com>
2014-08-11 19:57:58 -0700

    I created bank form and company and person controller and dialog

---
+++ src/View/Group7/Bank/Bank.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package View.Group7.Bank;
+
+import View.Group7.Bank.Controller.AddCompanyController;
+import View.Group7.Bank.Controller.AddPersonController;
+import com.asd.group7.common.gui.AccountFrm;
+import com.asd.group7.common.gui.controller.AccountController;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.UIManager;
+
+/**
+ *
+ * @author naimi_000
+ */
+public class Bank extends AccountFrm {
+
+    public Bank() {
+        setTitle("Bank Application");
+
+        //remove all listener that we have first
+        for (ActionListener al : JButton_PerAC.getActionListeners()) {
+            JButton_PerAC.removeActionListener(al);
+        }
+        for (ActionListener al : JButton_CompAC.getActionListeners()) {
+            JButton_CompAC.removeActionListener(al);
+        }
+
+        JButton_CompAC.addActionListener(new AddCompanyController());
+        JButton_PerAC.addActionListener(new AddPersonController());
+        
+    }
+
+    static public void main(String args[]) {
+        try {
+            // Add the following code if you want the Look and Feel
+            // to be set to the Look and Feel of the native system.
+
+            try {
+                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            } catch (Exception e) {
+            }
+
+            //Create a new instance of our application's frame, and make it visible.
+            (new Bank()).setVisible(true);
+        } catch (Throwable t) {
+            t.printStackTrace();
+            //Ensure the application exits with an error condition.
+            System.exit(1);
+        }
+    }
+}
---
+++ src/View/Group7/Bank/Dailog/CompanyDialog.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package View.Group7.Bank.Dailog;
+
+import com.asd.group7.common.gui.AccountFrm;
+import com.asd.group7.common.gui.JDialog_AddAccount;
+
+/**
+ *
+ * @author naimi_000
+ */
+public class CompanyDialog extends JDialog_AddAccount{
+
+    public CompanyDialog(AccountFrm parent) {
+        super(parent);
+        setTitle("Add Company Account");
+    }
+    
+    
+}
